ce65ea59dfded9d14fc0805e443e7457804eed75
Securitree <51289040+Securitree@users.noreply.github.com>
2020-12-01 23:38:27 +0000

    Add files via upload

---
+++ day1/day1_2.py
@@ -0,0 +1,31 @@
+input_file = "input.txt"
+total = 2020
+number_list = []
+number_set = set()
+
+
+with open (input_file,"r") as f:
+	for line in f:
+		number_list.append(int(line)) 
+        number_set.add(int(line))
+
+def part_one(number_list):
+	for number in number_list:
+		tmp = total - number
+		if tmp in number_set:
+			return = tmp * number
+
+
+def part_two(number_list):
+	for i in range (0,len(number_list)):
+		tmp_one = total - number_list[i]
+		for j in range (i+1,len(number_list)):
+			tmp = tmp_one - number_list[j]
+			if tmp in number_set:
+				return(tmp * number_list[i] * number_list[j])
+
+
+solution_one = part_one(number_list, number_set)
+print (solution_one)
+solution_two = part_two(number_list, number_set)
+print (solution_two)
